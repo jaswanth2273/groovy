89149d4d5dedc88db7d376dc8ead8c1159393e72
jaswanth2273 <64371373+jaswanth2273@users.noreply.github.com>
2022-04-18 14:50:51 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,4 +1,18 @@
 def call2() {
-  echo "Hello"
-  echo " working shared lib:"
+  
+  def DEV_USER = 'core'
+  def DEV_IP = '35.202.30.232'
+  def query=""
+  node {
+      parameters{
+          string(name: 'Enter_Query',defaultValue: '', description: '')
+      }
+      stage('run sql query') {
+          query=params.Enter_Query
+          if (query.contains('"')){
+              query=query.replaceAll('"','\\\\"')
+          }
+          sh """ssh ${DEV_USER}@${DEV_IP} 'kubectl exec -i data-mysql-0 -n core -- mysql -uroot -ptest@789 ${params.DB_Name} -e "${query}\\G"'"""
+      }
+  }
 }
